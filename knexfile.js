5c4581f415ad45d9226db2bd676fcc6813f08204
廖育醇 <dinoliao@local>
2022-07-28 18:20:55 +0800

    knex migration file OT-74

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      user: 'root',
+      password: 'hello_root',
+      database: 'order_form',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+}
---
+++ migrations/20220728073456_orderForm.js
@@ -0,0 +1,80 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (table) => {
+      table.primary('userId')
+      table.increments('userId').notNullable().comment('Primary Key')
+      table.datetime('createTime').notNullable().comment('建立時間')
+      table.datetime('updatetime').notNullable().comment('更新時間')
+      table.string('userName', 20).comment('使用者名稱')
+      table.string('userPassword', 64).comment('使用者密碼')
+      table.tinyint('adminPermission').comment('管理者權限')
+    })
+    .createTable('product', (table) => {
+      table.primary('productId')
+      table.increments('productId').notNullable().comment('Primary Key')
+      table.datetime('createTime').notNullable().comment('建立時間')
+      table.datetime('updatetime').notNullable().comment('更新時間')
+      table.string('productName', 20).notNullable().comment('商品名稱')
+      table.integer('productPrice').comment('商品價格')
+      table.integer('productSales').comment('已銷售數量')
+      table.integer('productStock').comment('庫存')
+      table.string('note').comment('備註')
+    })
+    .createTable('cart', (table) => {
+      table.primary(['userId', 'productId'])
+      table
+        .foreign('userId')
+        .references('users.userId')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+        .comment('使用者ID')
+      table
+        .foreign('productId')
+        .references('product.productId')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+        .comment('商品ID')
+      table.datetime('createTime').notNullable().comment('建立時間')
+      table.datetime('updatetime').notNullable().comment('更新時間')
+      table.integer('amount').notNullable().comment('數量')
+      table.integer('totalPrice').notNullable().comment('總價')
+    })
+    .createTable('order', (table) => {
+      table.primary('id')
+      table
+        .foreign('userId')
+        .references('users.userId')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+        .comment('使用者ID')
+      table.string('orderId', 36).notNullable().comment('訂單編號')
+      table
+        .foreign('productId')
+        .references('product.productId')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+        .comment('商品ID')
+      table.integer('amount').notNullable().comment('數量')
+      table.datetime('createTime').notNullable().comment('建立時間')
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable('users')
+    .dropTable('product')
+    .dropTable('cart')
+    .dropTable('order')
+}
