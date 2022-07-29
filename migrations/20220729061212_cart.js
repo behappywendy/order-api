2e95a8b5092cedccdfd927c2898be99060d20782
廖育醇 <dinoliao@local>
2022-07-29 15:19:10 +0800

    modify migration file OT-74

--- migrations/20220729061212_cart.js
+++ migrations/20220729061212_cart.js
@@ -4,14 +4,15 @@
  */
 exports.up = async (knex) => {
   const result = await knex.schema.createTable('cart', (table) => {
-    table.integer('userId').notNullable().primary().comment('使用者ID')
+    table.integer('userId').unsigned().notNullable().comment('使用者ID')
     table.foreign('userId').references('users.userId')
-    table.integer('productId').notNullable().primary().comment('商品ID')
+    table.integer('productId').unsigned().notNullable().comment('商品ID')
     table.foreign('productId').references('product.productId')
     table.datetime('createTime').notNullable().comment('建立時間')
     table.datetime('updatetime').notNullable().comment('更新時間')
     table.integer('amount').notNullable().comment('數量')
     table.integer('totalPrice').notNullable().comment('總價')
+    table.primary(['userId', 'productId'])
   })
   // .toSQL()
   console.log(result)
