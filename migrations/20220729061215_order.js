aae1db44f8ae569348ef8e4ca2118201feb013f5
廖育醇 <dinoliao@local>
2022-07-29 14:28:46 +0800

    modify migration file OT-74

--- migrations/20220729061215_order.js
+++ migrations/20220729061215_order.js
@@ -2,14 +2,36 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = async (knex) => {
+  const result = await knex.schema
+    .createTable('order', (table) => {
+      table.increments('id').notNullable().primary().comment('Primary Key')
+      table.integer('userId')
+      table
+        .foreign('userId')
+        .references('users.userId')
+        .onUpdate('NO ACTION')
+        .onDelete('NO ACTION')
+      table.string('orderId', 36).notNullable().comment('訂單編號')
+      table.integer('productId')
+      table
+        .foreign('productId')
+        .references('product.productId')
+        .onUpdate('NO ACTION')
+        .onDelete('NO ACTION')
+      table.integer('amount').notNullable().comment('數量')
+      table.datetime('createTime').notNullable().comment('建立時間')
+    })
+    .toSQL()
+  console.log(result)
+  return result
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = async (knex) => {
+  const result = await knex.schema.dropTable('order')
+  return result
+}
