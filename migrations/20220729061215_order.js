50aac80085985b3ba2609731682efb6ff0ad5d1c
廖育醇 <dinoliao@local>
2022-07-29 15:15:36 +0800

    modify migration file OT-74

--- migrations/20220729061215_order.js
+++ migrations/20220729061215_order.js
@@ -3,24 +3,23 @@
  * @returns { Promise<void> }
  */
 exports.up = async (knex) => {
-  const result = await knex.schema
-    .createTable('order', (table) => {
-      table.increments('id').notNullable().primary().comment('Primary Key')
-      table.integer('userId').unsigned().notNullable()
-      table.foreign('userId').references('users.userId')
-      // .onUpdate('NO ACTION')
-      // .onDelete('NO ACTION')
-      table.string('orderId', 36).notNullable().comment('訂單編號')
-      // table.integer('productId')
-      // table
-      //   .foreign('productId')
-      //   .references('product.productId')
-      //   .onUpdate('NO ACTION')
-      //   .onDelete('NO ACTION')
-      table.integer('amount').notNullable().comment('數量')
-      table.datetime('createTime').notNullable().comment('建立時間')
-    })
-    .toSQL()
+  const result = await knex.schema.createTable('order', (table) => {
+    table.increments('id').notNullable().primary().comment('Primary Key')
+    table.integer('userId').unsigned().notNullable()
+    table.foreign('userId').references('users.userId')
+    // .onUpdate('NO ACTION')
+    // .onDelete('NO ACTION')
+    table.string('orderId', 36).notNullable().comment('訂單編號')
+    // table.integer('productId')
+    // table
+    //   .foreign('productId')
+    //   .references('product.productId')
+    //   .onUpdate('NO ACTION')
+    //   .onDelete('NO ACTION')
+    table.integer('amount').notNullable().comment('數量')
+    table.datetime('createTime').notNullable().comment('建立時間')
+  })
+  // .toSQL()
   console.log(result)
   return result
 }
