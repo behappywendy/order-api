96a546f0fae9e7e150754c0dc323bf55c451d243
廖育醇 <dinoliao@local>
2022-07-29 15:09:23 +0800

    modify migration file OT-74

--- migrations/20220729061215_order.js
+++ migrations/20220729061215_order.js
@@ -5,19 +5,19 @@
 exports.up = async (knex) => {
   const result = await knex.schema.createTable('order', (table) => {
     table.increments('id').notNullable().primary().comment('Primary Key')
-    table.integer('userId')
+    table.integer('userId').notNullable()
     table
       .foreign('userId')
       .references('users.userId')
       .onUpdate('NO ACTION')
       .onDelete('NO ACTION')
     table.string('orderId', 36).notNullable().comment('訂單編號')
-    table.integer('productId')
-    table
-      .foreign('productId')
-      .references('product.productId')
-      .onUpdate('NO ACTION')
-      .onDelete('NO ACTION')
+    // table.integer('productId')
+    // table
+    //   .foreign('productId')
+    //   .references('product.productId')
+    //   .onUpdate('NO ACTION')
+    //   .onDelete('NO ACTION')
     table.integer('amount').notNullable().comment('數量')
     table.datetime('createTime').notNullable().comment('建立時間')
   })
