a4f8e6ff7867aee83e53bc2f0540d7a29059332d
廖育醇 <dinoliao@local>
2022-07-29 14:45:32 +0800

    modify migration file OT-74

--- migrations/20220729061212_cart.js
+++ migrations/20220729061212_cart.js
@@ -3,18 +3,22 @@
  * @returns { Promise<void> }
  */
 exports.up = async (knex) => {
-  const result = await knex.schema.createTable('cart', (table) => {
-    table.integer('userId').notNullable().comment('使用者ID')
-    table.integer('productId').notNullable().comment('商品ID')
-    table.foreign('userId').references('userId').inTable('users')
-    table.foreign('productId').references('productId').inTable('product')
-    table.datetime('createTime').notNullable().comment('建立時間')
-    table.datetime('updatetime').notNullable().comment('更新時間')
-    table.integer('amount').notNullable().comment('數量')
-    table.integer('totalPrice').notNullable().comment('總價')
-    table.primary(['userId', 'productId'])
-  })
-  // .toSQL()
+  const result = await knex.schema
+    .createTable('cart', (table) => {
+      table.integer('userId').notNullable().comment('使用者ID')
+      table.foreign('userId').references('users.userId').deferrable('deferred')
+      table.integer('productId').notNullable().comment('商品ID')
+      table
+        .foreign('productId')
+        .references('product.productId')
+        .deferrable('deferred')
+      table.datetime('createTime').notNullable().comment('建立時間')
+      table.datetime('updatetime').notNullable().comment('更新時間')
+      table.integer('amount').notNullable().comment('數量')
+      table.integer('totalPrice').notNullable().comment('總價')
+      table.primary(['userId', 'productId'])
+    })
+    .toSQL()
   console.log(result)
   return result
 }
