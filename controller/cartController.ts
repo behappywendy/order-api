4217b5e5203f0cc2ce05a247bc53f4a243022c9a
廖育醇 <dinoliao@local>
2022-08-04 15:19:16 +0800

    cart api test file OT-73

--- controller/cartController.ts
+++ controller/cartController.ts
@@ -9,38 +9,61 @@ export const createCart = async (req: Request, res: Response) => {
   const userId = getUserFromJwt(token!)
   const nowTime = moment.tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss')
 
-  if (userId) {
-    const cartExist = await knex('cart')
-      .select()
-      .where('userId', userId)
-      .andWhere('productId', productId)
+  const productExist = await knex('product')
+    .select()
+    .where('productId', productId)
 
-    if (cartExist.length) {
-      knex('cart')
+  if (!productExist.length)
+    res.status(404).json({
+      message: '無此商品',
+    })
+  else {
+    if (userId) {
+      const cartExist = await knex('cart')
+        .select()
         .where('userId', userId)
         .andWhere('productId', productId)
-        .update({
-          updateTime: nowTime,
-          amount,
-          totalPrice,
-        })
-        .then((result: any) => {
-          res.status(!!result ? 200 : 404).json({ success: !!result })
-        })
-    } else {
-      knex('cart')
-        .insert({
-          createTime: nowTime,
-          updateTime: nowTime,
-          userId,
-          productId,
-          amount,
-          totalPrice,
-        })
-        .then((result: any) => {
-          res.send(result)
-        })
-    }
+
+      if (cartExist.length) {
+        knex('cart')
+          .where('userId', userId)
+          .andWhere('productId', productId)
+          .update({
+            updateTime: nowTime,
+            amount,
+            totalPrice,
+          })
+          .then((result: any) => {
+            res.send({
+              userId,
+              productId,
+              amount,
+              totalPrice,
+            })
+          })
+      } else {
+        knex('cart')
+          .insert({
+            createTime: nowTime,
+            updateTime: nowTime,
+            userId,
+            productId,
+            amount,
+            totalPrice,
+          })
+          .then((result: any) => {
+            res.send({
+              userId,
+              productId,
+              amount,
+              totalPrice,
+            })
+          })
+      }
+    } else
+      res.status(401).json({
+        message: '尚未登入',
+      })
   }
 }
 
@@ -62,7 +85,10 @@ export const readCart = (req: Request, res: Response) => {
       .then((result: any) => {
         res.send(result)
       })
-  }
+  } else
+    res.status(401).json({
+      message: '尚未登入',
+    })
 }
 
 export const updateCart = (req: Request, res: Response) => {
@@ -85,7 +111,10 @@ export const updateCart = (req: Request, res: Response) => {
         res.status(!!result ? 200 : 404).json({ success: !!result })
       })
       .catch((error: any) => res.status(500).json(error))
-  }
+  } else
+    res.status(401).json({
+      message: '尚未登入',
+    })
 }
 
 export const deleteCart = (req: Request, res: Response) => {
@@ -112,5 +141,8 @@ export const deleteCart = (req: Request, res: Response) => {
         })
         .catch((error: any) => res.status(500).json(error))
     }
-  }
+  } else
+    res.status(401).json({
+      message: '尚未登入',
+    })
 }
