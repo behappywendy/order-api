41f54675455273f53fe2b5938d6916a75f1a6a59
廖育醇 <dinoliao@local>
2022-08-05 16:54:25 +0800

    fix index.ts conflict OT-74

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 import productRouter from './router/productRouter';
 import userRouter from './router/userRouter';
@@ -7,21 +8,29 @@ import orderRouter from './router/orderRouter';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const swaggerDocument = require('../swagger.json');
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', cors(), (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('123');
 });
 app.use(
   cors({
-    origin: `http://localhost:8080`,
+    // origin: 'http://localhost:8080',
+    origin: '*',
   })
 );
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/product', productRouter);
 app.use('/user', userRouter);
 app.use('/cart', cartRouter);
 app.use('/order', orderRouter);
+
+app.get('/test', (req, res) => {
+  res.send(req.headers);
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
