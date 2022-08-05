25253916b971cdbc42e4f838bafdebe61df781b5
廖育醇 <dinoliao@local>
2022-08-05 16:50:18 +0800

    fix order controller confilct OT-73

--- controller/orderController.ts
+++ controller/orderController.ts
@@ -1,105 +1,121 @@
-import { knex } from './../model/mysql'
-import { Request, Response } from 'express'
-import { getUserFromJwt } from '../model/jwt'
-import { v4 as uuidv4 } from 'uuid'
-import moment from 'moment-timezone'
+import { knex } from './../model/mysql';
+import { Request, Response } from 'express';
+import { getUserFromJwt } from '../model/jwt';
+import { v4 as uuidv4 } from 'uuid';
+import moment from 'moment-timezone';
+import client from '../model/redis';
 
-export const createOrder = async (req: Request, res: Response) => {
-  const orderId = uuidv4()
-  const token = req.headers.authorization?.split(' ')[1]
-  const userId = getUserFromJwt(token!)
-  const orderList = req.body
-  const nowTime = moment.tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss')
-
-  if (orderList.length) {
-    if (userId) {
-      for await (const { productId, amount } of orderList) {
-        knex('order')
-          .insert({
-            createTime: nowTime,
-            userId,
-            orderId,
-            productId,
-            amount,
-          })
-          .then((result: any) => {
-            console.log(result)
-          })
-      }
-      res.send({ orderId })
-    } else
-      res.status(401).json({
-        message: '尚未登入',
-      })
-  } else
-    res.status(404).json({
-      message: '輸入的資料不是陣列',
-    })
-}
+const KEY = 'orderList';
+const expireTime = 60 * 60 * 24;
 
 type GetOrder = {
-  orderId: string
-  productId: number
-  productName: string
-  productPrice: number
-  amount: number
-  createTime: string
-}
+  orderId: string;
+  productId: number;
+  productName: string;
+  productPrice: number;
+  amount: number;
+  createTime: string;
+};
+
+const loadOrder = async (update: boolean, userId: number) => {
+  const oldData = await client.get(`user_${userId}_${KEY}`);
+  if (!!oldData && !update) return JSON.parse(oldData);
+
+  const result = await knex('order')
+    .join('product', 'order.productId', '=', 'product.productId')
+    .select(
+      'order.orderId',
+      'product.productId',
+      'product.productName',
+      'product.productPrice',
+      'order.amount',
+      'order.createTime'
+    )
+    .where('userId', userId);
+
+  result.forEach((element: GetOrder) => {
+    element.createTime = moment(element.createTime).format(
+      'YYYY-MM-DD HH:mm:ss'
+    );
+  });
+
+  const data = [
+    {
+      orderId: result[0].orderId,
+      content: [
+        {
+          productId: result[0].productId,
+          productName: result[0].productName,
+          productPrice: result[0].productPrice,
+          amount: result[0].amount,
+          createTime: result[0].createTime,
+        },
+      ],
+    },
+  ];
+
+  for (let i = 1; i < result.length; i++) {
+    let same = -1;
+    for (let j = 0; j < data.length; j++) {
+      if (result[i].orderId === data[j].orderId) {
+        same = j;
+      }
+    }
+    if (same === -1) {
+      data.push({
+        orderId: result[i].orderId,
+        content: [
+          {
+            productId: result[i].productId,
+            productName: result[i].productName,
+            productPrice: result[i].productPrice,
+            amount: result[i].amount,
+            createTime: result[i].createTime,
+          },
+        ],
+      });
+    } else {
+      data[same].content.push({
+        productId: result[i].productId,
+        productName: result[i].productName,
+        productPrice: result[i].productPrice,
+        amount: result[i].amount,
+        createTime: result[i].createTime,
+      });
+    }
+  }
+
+  client.set(`user_${userId}_${KEY}`, JSON.stringify(data), { EX: expireTime });
+  return data;
+};
+
+export const createOrder = async (req: Request, res: Response) => {
+  const orderId = uuidv4();
+  const token = req.headers.authorization?.split(' ')[1];
+  const userId = getUserFromJwt(token!);
+  const orderList = req.body;
+  const nowTime = moment.tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss');
+
+  for await (const { productId, amount } of orderList) {
+    knex('order')
+      .insert({
+        createTime: nowTime,
+        userId,
+        orderId,
+        productId,
+        amount,
+      })
+      .then((result: any) => {
+        console.log(result);
+      });
+  }
+  loadOrder(true, userId!);
+  res.send(orderId);
+};
 
 export const readOrder = async (req: Request, res: Response) => {
-  const token = req.headers.authorization?.split(' ')[1]
-  const userId = getUserFromJwt(token!)
-
-  if (userId) {
-    knex('order')
-      .join('product', 'order.productId', '=', 'product.productId')
-      .select(
-        'order.orderId',
-        'product.productId',
-        'product.productName',
-        'product.productPrice',
-        'order.amount',
-        'order.createTime'
-      )
-      .where('userId', userId)
-      .then((result: GetOrder[]) => {
-        result.forEach((element: GetOrder) => {
-          element.createTime = moment(element.createTime).format(
-            'YYYY-MM-DD HH:mm:ss'
-          )
-        })
-
-        const map = new Map()
-        for (let i = 0; i < result.length; i++) {
-          const element = result[i]
-          const isSame = map.has(element.orderId)
-          if (isSame) {
-            map.set(result[i].orderId, [
-              ...map.get(result[i].orderId),
-              {
-                createTime: result[i].createTime,
-                productId: result[i].productId,
-                productName: result[i].productName,
-                productPrice: result[i].productPrice,
-                amount: result[i].amount,
-              },
-            ])
-          } else {
-            map.set(result[i].orderId, [
-              {
-                createTime: result[i].createTime,
-                productId: result[i].productId,
-                productName: result[i].productName,
-                productPrice: result[i].productPrice,
-                amount: result[i].amount,
-              },
-            ])
-          }
-        }
-        res.send(Object.fromEntries(map))
-      })
-  } else
-    res.status(401).json({
-      message: '尚未登入',
-    })
-}
+  const token = req.headers.authorization?.split(' ')[1];
+  const userId = getUserFromJwt(token!);
+  const data = await loadOrder(false, userId!);
+  res.send(data);
+};
