d84da7f166138f5835d54b8951088550a362c9e0
廖育醇 <dinoliao@local>
2022-08-05 16:55:18 +0800

    fix index.ts conflict OT-74

--- index.ts
+++ index.ts
@@ -1,37 +1,37 @@
-import express, { Express, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import cors from 'cors';
-import productRouter from './router/productRouter';
-import userRouter from './router/userRouter';
-import cartRouter from './router/cartRouter';
-import orderRouter from './router/orderRouter';
-import dotenv from 'dotenv';
-dotenv.config();
+import express, { Express, Request, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import cors from 'cors'
+import productRouter from './router/productRouter'
+import userRouter from './router/userRouter'
+import cartRouter from './router/cartRouter'
+import orderRouter from './router/orderRouter'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const swaggerDocument = require('../swagger.json');
-const app: Express = express();
-const port = process.env.PORT;
+const swaggerDocument = require('../swagger.json')
+const app: Express = express()
+const port = process.env.PORT
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('123');
-});
+  res.send('123')
+})
 app.use(
   cors({
     // origin: 'http://localhost:8080',
     origin: '*',
   })
-);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use('/product', productRouter);
-app.use('/user', userRouter);
-app.use('/cart', cartRouter);
-app.use('/order', orderRouter);
+app.use('/product', productRouter)
+app.use('/user', userRouter)
+app.use('/cart', cartRouter)
+app.use('/order', orderRouter)
 
 app.get('/test', (req, res) => {
-  res.send(req.headers);
-});
+  res.send(req.headers)
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
