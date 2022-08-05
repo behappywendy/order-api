a5ecae441717809da1fb27bb5bcfee5aae7b96b4
廖育醇 <dinoliao@local>
2022-08-05 16:53:03 +0800

    fix index.ts conflict OT-74

--- index.ts
+++ index.ts
@@ -1,29 +1,28 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import productRouter from './router/productRouter'
-import userRouter from './router/userRouter'
-import cartRouter from './router/cartRouter'
-import orderRouter from './router/orderRouter'
-import dotenv from 'dotenv'
-dotenv.config()
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import productRouter from './router/productRouter';
+import userRouter from './router/userRouter';
+import cartRouter from './router/cartRouter';
+import orderRouter from './router/orderRouter';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app: Express = express()
-// const port = process.env.PORT
-const port = 3001
+const app: Express = express();
+const port = process.env.PORT;
 
 app.get('/', cors(), (req: Request, res: Response) => {
-  res.send('123')
-})
+  res.send('123');
+});
 app.use(
   cors({
     origin: `http://localhost:8080`,
   })
-)
-app.use('/product', productRouter)
-app.use('/user', userRouter)
-app.use('/cart', cartRouter)
-app.use('/order', orderRouter)
+);
+app.use('/product', productRouter);
+app.use('/user', userRouter);
+app.use('/cart', cartRouter);
+app.use('/order', orderRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
